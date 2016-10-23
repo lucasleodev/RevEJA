bb7fdbb92568ef3e0232e51a0ab3ec2e6ae9a3f3
lucasleoxavier <lucasxavier92@gmail.com>
2016-10-23 15:38:45 -0200

    Fullscreen Toggle funcionando
    Agora o toggle alterna entre tela cheia ou não

--- Assets/_project/scripts/menu/configsGame.cs
+++ Assets/_project/scripts/menu/configsGame.cs
@@ -1,21 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class configsGame : MonoBehaviour {
 
     public GameObject botaoVoltarMenu;
+    public Toggle botaoTelaCheia;
 
 
 	// Use this for initialization
 	void Start () {
-	
+
+        //se o jogo iniciar em tela cheia, o toggle fica ativado, se não fica desativado
+	if(Screen.fullScreen == true)
+        {
+            botaoTelaCheia.isOn = true;
+        }
+    else
+        {
+            botaoTelaCheia.isOn = false;
+        }
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    public void Resolucao()
+    {
+
+    }
+
+    public void TelaCheia()
+    {
+        
+        if (Screen.fullScreen == true)
+        {
+            Screen.fullScreen = false;
+        }
+        else
+        {
+            Screen.fullScreen = true;
+        }
+    }
 
     public void VoltarAoMenu()
     {
