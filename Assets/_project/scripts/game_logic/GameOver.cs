8972e80374272d5d49324d9cfd996cf9769bcd32
lucasleoxavier <lucasxavier92@gmail.com>
2016-11-18 11:15:22 -0200

    Inserção da tela de Game Over
    game over

---
+++ Assets/_project/scripts/game_logic/GameOver.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour {
+
+    public Text lbModoJogo;
+    public Text lbAcertos;
+    public Text lbErros;
+    public Text lbEstatisticas;    
+
+    logicaJogo loJogo;
+
+    // Use this for initialization
+    void Start () {
+        loJogo = GetComponent<logicaJogo>();
+        DefinirModoJogo();
+        lbAcertos.text = ("Você acertou " + loJogo.acertos + " de 10");
+        lbAcertos.text = ("Você errou " + loJogo.acertos);
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+
+    public void DefinirEstatisticas()
+    {
+        float media = loJogo.acertos / 10;
+    }
+
+    public void DefinirModoJogo()
+    {
+        int mJogo = logicaJogo.modoJogo;
+        switch (mJogo)
+        {
+            case 0:
+                lbModoJogo.text = "Modo Ampulheta";
+                break;
+            case 1:
+                lbModoJogo.text = "Matemática - Nível Básico";
+                break;
+            case 2:
+                lbModoJogo.text = "Matemática - Nível Avançado";
+                break;
+            case 3:
+                lbModoJogo.text = "Português - Nível Básico";
+                break;
+            case 4:
+                lbModoJogo.text = "Português - Nível Avançado";
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    public void SairGameOver()
+    {
+        SceneManager.LoadScene("mainMenu");  
+    }
+}
