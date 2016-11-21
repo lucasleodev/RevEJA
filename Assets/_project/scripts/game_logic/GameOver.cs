75f9ac38fb872224c6a5218aabab33c5c28f9b73
lucasleoxavier <lucasxavier92@gmail.com>
2016-11-21 21:14:38 -0200

    Agora arrumou
    aehoo

--- Assets/_project/scripts/game_logic/GameOver.cs
+++ Assets/_project/scripts/game_logic/GameOver.cs
@@ -12,10 +12,11 @@ public class GameOver : MonoBehaviour {
 
     public Text lbModoJogo;
     public Text lbAcertos;
-    public Text lbErros;   
+    public Text lbErros;
+    public Text lbMedia;   
 
     public double media = 0.0f;
-    public int modo;
+    public int modo, totalQuestoes;
 
     logicaJogo loJogo;
 
@@ -24,9 +25,10 @@ public class GameOver : MonoBehaviour {
         SomGameOver();
         loJogo = GetComponent<logicaJogo>();
         DefinirModoJogo();
-       // DefinirEstatisticas();
-        lbAcertos.text = "Você acertou "+ logicaJogo.qtdAcerto+" de 10";
-        lbErros.text = "Você errou "+logicaJogo.qtdErro;        
+        DefinirEstatisticas();
+        lbAcertos.text = "Você acertou "+ logicaJogo.qtdAcerto+" de "+ totalQuestoes;
+        lbErros.text = "Você errou "+logicaJogo.qtdErro;
+        lbMedia.text = "Sua média de acerto foi de " + media + "%";        
     }
 
     // Update is called once per frame
@@ -41,24 +43,40 @@ public class GameOver : MonoBehaviour {
         {
             case 0:
                 lbModoJogo.text = "Modo Ampulheta";
+                totalQuestoes = 40;
                 break;
             case 1:
                 lbModoJogo.text = "Matemática - Nível Básico";
+                totalQuestoes = 10;
                 break;
             case 2:
                 lbModoJogo.text = "Matemática - Nível Avançado";
+                totalQuestoes = 10;
                 break;
             case 3:
                 lbModoJogo.text = "Português - Nível Básico";
+                totalQuestoes = 10;
                 break;
             case 4:
                 lbModoJogo.text = "Português - Nível Avançado";
+                totalQuestoes = 10;
                 break;
             default:
                 break;
         }
 
     }    
+    public void DefinirEstatisticas()
+    {
+        if(modo==0)
+        {
+            media = (logicaJogo.qtdAcerto / 40.0f) * 100.0f;
+        }
+        else
+        {
+            media = (logicaJogo.qtdAcerto / 10.0f) * 100.0f;
+        }
+    }
 
     public void SairGameOver()
     {
